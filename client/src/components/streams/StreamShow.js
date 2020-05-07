356e7ffd279c8f0e7372a018090de0a6e13dcb37
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-06 23:07:34 -0700

    update Stream Show

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -9,9 +9,16 @@ class StreamShow extends Component {
     }
 
     render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>;
+        }
+
+        const { title, description } = this.props.stream;
+
         return (
             <div>
-                {this.props.stream ? this.props.stream.title : "Loading..."}
+                <h1>{title}</h1>
+                <h5>{description}</h5>
             </div>
         );
     };
