b4d242e80d8ed7490eccbb7ad4b18b98c55b7e23
Adrian Ramos <ramosadrian56@gmail.com>
2020-05-06 22:07:11 -0700

    add intentional navigation to StreamDelete.js

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStream, deleteStream } from '../../actions';
 
 
@@ -10,31 +11,29 @@ class StreamDelete extends Component {
         this.props.fetchStream(this.props.match.params.id);
     }
 
-    onClickDelete = () => {
-        this.props.deleteStream(this.props.stream.id);
-    };
+    renderActions() {
+        const { id } = this.props.match.params;
 
-
-    render() {
-        const actions = (
+        return (
             <React.Fragment>
-                <button className="ui button negative" onClick={() => this.onClickDelete()}>
+                <button className="ui button negative" onClick={() => this.props.deleteStream(id)}>
                     Delete Stream
                 </button>
-                <button className="ui button">
+                <Link to="/" className="ui button">
                     Cancel
-                </button>
+                </Link>
             </React.Fragment>
-        );
-        
-        console.log(this.props.stream)
+        )    
+    };
+
+    render() {
         return (
             <div>
                 StreamDelete
                 <Modal 
                     title="Delete Stream"
                     content={this.props.stream ? this.props.stream.title : "Loading..."}
-                    actions={actions}
+                    actions={this.renderActions()}
                     onDismiss={() => history.push("/")}
                 />
             </div>
